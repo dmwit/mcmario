0233883c70b89b3151ae76a3de6630c2a87ed1fb
Daniel Wagner <dmwit@galois.com>
2017-07-18 18:37:33 -0700

    UI for adding players and games

--- static/ui.js
+++ static/ui.js
@@ -1,14 +1,19 @@
 var nextSide = "left";
 var mostRecentMatchup = undefined;
 var levelMap = {};
+var shouldRefreshNameList = false;
 $(setUp);
 
 function setUp() {
 	refreshNameList();
-	$( "#left .clear").click(function() { clearPlayer("left" ); });
-	$("#right .clear").click(function() { clearPlayer("right"); });
+	$(" #left .name ").click("left" , recordGame);
+	$("#right .name ").click("right", recordGame);
+	$( "#left .clear").click("left" , clearPlayer);
+	$("#right .clear").click("right", clearPlayer);
+	// TODO: bind is deprecated in favor of on
 	$(" #left .level").bind("keyup input", "left" , synchronizeLevel);
 	$("#right .level").bind("keyup input", "right", synchronizeLevel);
+	$("#new-player form").on("submit", addName);
 }
 
 function refreshNameList() {
@@ -26,6 +31,19 @@ function refreshNameList() {
 }
 
 function selectName() {
+	queueName(this.textContent);
+}
+
+function addName() {
+	var name = $("#new-player .name").prop("value");
+	// Just to filter out mistaken clicks. The serve itself doesn't care what
+	// name you pick.
+	if(name.match(/[a-zA-Z]/)) queueName(name);
+	shouldRefreshNameList = true;
+	return false;
+}
+
+function queueName(name) {
 	var side;
 	if($("#left .name").hasClass("uninitialized"))
 		side = "left";
@@ -35,10 +53,11 @@ function selectName() {
 		side = nextSide;
 
 	nextSide = otherSide(side);
-	here  = $("#" + side     + " .name");
-	there = $("#" + nextSide + " .name");
+	var here  = $("#" + side     + " .name");
+	var there = $("#" + nextSide + " .name");
 
-	here.text(this.textContent).removeClass("uninitialized");
+	here.text(name).removeClass("uninitialized");
+	$("#" + side + " .clear").removeClass("disabled");
 	refreshMatchup();
 }
 
@@ -49,20 +68,30 @@ function refreshMatchup() {
 		return;
 
 	var path = "/matchup/" + left.text() + "/" + right.text();
+	clearSelectors();
 	disableSelectors();
 	mostRecentMatchup = $.get(path, setMatchup);
 }
 
-function clearPlayer(side) {
+function clearPlayer(ev) {
+	var side = ev.data;
 	$("#" + side + " .name").text("???").addClass("uninitialized");
+	$("#" + side + " .clear").addClass("disabled");
+	clearSelectors();
 	disableSelectors();
 }
 
+function clearSelectors() {
+	$(".player .speed option[value=Low]").prop("selected", true);
+	$(".player .level").prop("value", "");
+	$("#sync").prop("checked", false);
+	$("#game-record-error-box").addClass("hidden");
+}
+
 function disableSelectors() {
-	$(".player .speed option[value=Low]").attr("selected", "true");
 	$(".player .speed").prop("disabled", true);
-	$(".player .level").prop("disabled", true).attr("value", "");
-	$("#sync").prop("checked", false).prop("disabled", true);
+	$(".player .level").prop("disabled", true);
+	$("#sync").prop("disabled", true);
 }
 
 function enableSelectors() {
@@ -73,10 +102,10 @@ function enableSelectors() {
 
 function setMatchup(data, result, jqxhr) {
 	if(jqxhr !== mostRecentMatchup) return;
-	$("#left  .speed option[value=" + data.left .speed + "]").attr("selected", "true");
-	$("#right .speed option[value=" + data.right.speed + "]").attr("selected", "true");
-	$("#left  .level").attr("value", data.left .level);
-	$("#right .level").attr("value", data.right.level);
+	$("#left  .speed option[value=" + data.left .speed + "]").prop("selected", true);
+	$("#right .speed option[value=" + data.right.speed + "]").prop("selected", true);
+	$("#left  .level").prop("value", data.left .level);
+	$("#right .level").prop("value", data.right.level);
 	$("#sync").prop("checked", data.confidence === "Confident");
 	levelMap["left" ] = data.leftToRight;
 	levelMap["right"] = data.rightToLeft;
@@ -91,6 +120,48 @@ function synchronizeLevel(ev) {
 	var there = otherSide(here);
 	var levelThere = levelMap[here][levelHere];
 	$("#" + there + " .level").prop("value", levelThere);
+}
+
+function recordGame(ev) {
+	if($("#left  .name").hasClass("uninitialized") ||
+	   $("#right .name").hasClass("uninitialized"))
+		return;
+	disableSelectors();
+	$(".clear").addClass("disabled");
+
+	var winner = ev.data, loser = otherSide(winner);
+	var postData =
+		{ winner: JSON.stringify(playerSettings(winner))
+		, loser : JSON.stringify(playerSettings(loser ))
+		};
+	// TODO: why are we getting an XML parsing error every time we record a game?
+	$.post("/game", postData, gameRecorded, "text").fail(gameNotRecorded);
+}
+
+function gameRecorded() {
+	clearPlayer({ data: "left" });
+	clearPlayer({ data: "right"});
+	if(shouldRefreshNameList) {
+		refreshNameList();
+		shouldRefreshNameList = false;
+	}
+}
+
+function gameNotRecorded() {
+	enableSelectors();
+	$(".clear").removeClass("disabled");
+	$("#game-record-error-box").removeClass("hidden");
+}
+
+function playerSettings(side) {
+	// not just return { ... } because the semicolon insertion algorithm gets
+	// it wrong
+	var result =
+		{ name:           $("#" + side + " .name" ).text()
+		, level: parseInt($("#" + side + " .level").prop("value"))
+		, speed:          $("#" + side + " .speed").prop("value")
+		};
+	return result;
 }
 
 function otherSide(here) { return (here === "left") ? "right" : "left"; }
